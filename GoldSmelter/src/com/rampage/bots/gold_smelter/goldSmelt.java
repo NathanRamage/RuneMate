99b382024363f76ec1d116d1947a6ecff88eed2a
NathanRamage <nramage94@gmail.com>
2016-03-27 21:44:21 +1000

    initial commit

---
+++ GoldSmelter/src/com/rampage/bots/gold_smelter/goldSmelt.java
@@ -0,0 +1,19 @@
+package com.rampage.bots.gold_smelter;
+
+import com.runemate.game.api.script.framework.LoopingScript;
+
+/**
+ * Created by nathan on 27/03/16.
+ */
+public class goldSmelt extends LoopingScript {
+
+    @Override
+    public void onStart(String... a) {
+        System.out.println("This is a bot");
+    }
+
+    @Override
+    public void onLoop() {
+
+    }
+}
